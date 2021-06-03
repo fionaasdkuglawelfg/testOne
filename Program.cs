b3118302a8d52349c1e4aeb4995c98adcb066f5f
fionaasdkuglawelfg <85283007+fionaasdkuglawelfg@users.noreply.github.com>
2021-06-03 10:50:04 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZenerdiodeAufgabe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Zenerdiode.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZenerdiodeAufgabe
+{
+    class Zenerdiode
+    {
+        //Variablen
+        private double voltageUz;
+        private double powerPztot;
+        private double currentIztot;
+        private double currentIzmin;
+        private double currentIzmax;
+        private double currentIz;
+
+
+        //Konstruktor
+        public Zenerdiode(double spannungUz, double leistungPztot)
+        {
+            voltageUz = spannungUz;
+            powerPztot = leistungPztot;
+            currentIztot = (spannungUz / leistungPztot);
+            currentIzmax = (0.9 * currentIztot);
+            currentIzmin = (0.1 * currentIztot);
+            currentIz = (0.5 * currentIztot);
+        }
+        public void setCurrent (double StromIz)
+        {
+
+        }
+        public double getVoltageUz()
+        {
+
+        }
+        public double getPowerPz()
+        {
+
+        }
+        public double getCurrentIztot()
+        {
+
+        }
+        public double getCurrentIzmin()
+        {
+
+        }
+        public double getCurrentIzmax()
+        {
+
+        }
+        public bool testWorkingArea()
+        {
+
+        }
+    }
+}
